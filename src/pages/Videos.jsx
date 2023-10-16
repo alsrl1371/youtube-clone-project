0c4782d27c17822a37a1a0cc7cc1409d3f332e68
kimminki <kimminki02@naver.com>
2023-10-16 15:24:02 +0900

    refactor: fetch 코드 리팩토링

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import VideoCard from '../components/VideoCard';
+import axios from 'axios';
 
 export default function Videos() {
   const { keyword } = useParams();
@@ -10,9 +11,9 @@ export default function Videos() {
     error,
     data: videos,
   } = useQuery(['videos', keyword], async () => {
-    return fetch(`/videos/${keyword ? 'search' : 'popular'}.json`)
-      .then((res) => res.json())
-      .then((data) => data.items);
+    return axios
+      .get(`/videos/${keyword ? 'search' : 'popular'}.json`)
+      .then((res) => res.data.items);
   });
 
   // 데이터가 아직 로딩 중이면 Loading 표시
