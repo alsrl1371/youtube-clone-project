05ebadc93730ae124ad06d3737c96f9630cc0e70
kimminki <kimminki02@naver.com>
2023-10-20 07:33:15 +0900

    feat: VideoDetail 구현

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { formatAgo } from '../util/data';
+import { useNavigate } from 'react-router-dom';
 
 export default function VideoCard({ video }) {
   const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  const navigate = useNavigate();
   return (
-    <li>
+    <li
+      onClick={() => {
+        navigate(`videos/watch/${video.id}`, { state: { video } });
+      }}
+    >
       <img className='w-full' src={thumbnails.medium.url} alt={title} />
       <div>
         <p className='my-2 font-semibold line-clamp-2'>{title}</p>
--- src/pages/VideoDetail.jsx
+++ src/pages/VideoDetail.jsx
@@ -1,5 +1,34 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
+import ChannelInfo from '../components/ChannelInfo';
+import RelatedVideos from '../components/RelatedVideos';
 
 export default function VideoDetail() {
-  return <div>detail</div>;
+  const {
+    state: { video },
+  } = useLocation();
+  const { title, channelId, channelTitle, description } = video.snippet;
+  return (
+    <section>
+      <article>
+        <iframe
+          id='player'
+          type='text/html'
+          width='640'
+          height='390'
+          src={`http://www.youtube.com/embed/${video.id}`}
+          title={video.title}
+          frameborder='0'
+        />
+        <div>
+          <h2>{title}</h2>
+          <ChannelInfo id={channelId} name={channelTitle} />
+          <pre>{description}</pre>
+        </div>
+      </article>
+      <section>
+        <RelatedVideos id={video.id} />
+      </section>
+    </section>
+  );
 }
