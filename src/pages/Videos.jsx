69dcb82338a10947260680a965db4a72458bc68d
kimminki <kimminki02@naver.com>
2023-10-23 14:05:49 +0900

    feat: 해당 채널의 비디오 목록 가져오기

--- src/pages/Videos.jsx
+++ src/pages/Videos.jsx
@@ -25,7 +25,6 @@ export default function Videos() {
 
   return (
     <>
-      <div>Videos {keyword ? `🔍${keyword}` : '✨'}</div>
       {videos && (
         <ul className='grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-y-4'>
           {videos.map((video) => (
